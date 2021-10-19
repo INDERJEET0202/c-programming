ecbf15b1dd12fba93c901bf7b599adf30f4a69be
indrajit <palindrajit10@gmail.com>
2021-10-19 16:43:16 +0530

    .

--- dynamic_memory_allocation.c
+++ dynamic_memory_allocation.c
@@ -10,7 +10,7 @@ int main()
     printf("Enter the size of array you want to create: ");
     scanf("%d", &n);
 
-    ptr = (int* ) malloc(n*sizeof(int));
+    ptr = (int* ) malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
     {
         printf("Enter the value number %d of this array : ", i+1);
