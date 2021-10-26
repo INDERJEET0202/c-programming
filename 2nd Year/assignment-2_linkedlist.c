90da45a72d29714e44421ae9195189fd0b503fb1
indrajit <palindrajit10@gmail.com>
2021-10-26 22:25:13 +0530

    .

--- 2nd Year/assignment-2_linkedlist.c
+++ 2nd Year/assignment-2_linkedlist.c
@@ -46,11 +46,12 @@ int search(int d, struct node * head){
 
 void main() 
 {
+
     struct node *head;
     struct node *p;
     head = (struct node *)malloc(sizeof(struct node));
     p = head;
-    int n;
+    int n, x;
     printf("Enter how many nodes you want - ");
     scanf("%d", &n);
     
@@ -76,19 +77,37 @@ void main()
     //     head = (insertAtEnd(head, dd));
     // }
 
-    linkedListPrint(head);
 
-    // int item;
-    // printf("Enter element to be searched: ");
-    // scanf("%d", &item);
-    // int index = search(item, head);
-    // if(index != -1){
-    //     printf("Element found at node %d\n", index+1);
-    // }
-    // else{
-    //     printf("Element not found.\n");
-    // }
-    // head = reverse(head);
-    // printf("After reverse: \n");
-    // linkedListPrint(head);
+    int s = 1;
+    int item;
+    while(s ==1){
+        printf("\nPress 1 to reverse the linkedlist.\nPress 2 for search in the linkedlist.\nPress 3 to Display \nPress 4 to EXIT.\n\n");
+        scanf("%d", &x);
+        switch(x){
+            case 1:
+                head = reverse(head);
+                printf("After reverse: \n");
+                linkedListPrint(head);
+                break;
+            case 2:
+                printf("Enter element to be searched: ");
+                scanf("%d", &item);
+                int index = search(item, head);
+                if(index != -1){
+                    printf("Element found at node %d\n", index+1);
+                }
+                else{
+                    printf("Element not found.\n");
+                }
+                break;
+            case 3:
+                linkedListPrint(head);
+                break;
+            case 4:
+                s = -1;
+                break;
+            default:
+                printf("Please choose a correct option.");
+        }
+    }
 }
