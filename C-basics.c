2b4fc6942fbdb4a9d220c484d22e2daec584f7e1
indrajit <palindrajit10@gmail.com>
2021-11-11 20:33:47 +0530

    trying github copilot

--- C-basics.c
+++ C-basics.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+int sum(int a, int b) {
+    return a + b;
+}
+
 int main(){
     // char name[20];
-
+    int a, b;
+    printf("Enter 1st number: ");
+    scanf("%d", &a);
+    printf("Enter 2nd number: ");
+    scanf("%d", &b);
+    int c = sum(a, b);
+    printf("Sum of %d and %d is %d\n", a, b, c);
     // printf("Enter name: ");
     // scanf("%s", &name);
     // printf("Entered name is %s", name);
