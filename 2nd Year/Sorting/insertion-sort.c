873ed6015660384b90fbb3fd970f7180a1d1875f
indrajit <palindrajit10@gmail.com>
2021-12-09 18:25:24 +0530

    sorting

--- 2nd Year/Sorting/insertion-sort.c
+++ 2nd Year/Sorting/insertion-sort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int key, j;
 void printArray(int A[], int n){
@@ -9,10 +10,10 @@ void printArray(int A[], int n){
 }
 
 void insertionSort(int a[], int n){
-    for(int i = 1; i <= (n - 1); i++){ // Loop for passes
+    for(int i = 1; i <= (n - 1); i++){
         key = a[i];
         j = i - 1;
-        while(j >= 0 && a[j] > key){ // Loop for each pass
+        while(j >= 0 && a[j] > key){
             a[j + 1] = a[j];
             j--;
         }
@@ -22,10 +23,24 @@ void insertionSort(int a[], int n){
 
 int main()
 {
-    int a[] = {12, 54, 65, 7, 23, 9};
-    int n = 6;
-    printArray(a, n); // Printing the array before sorting.
-    insertionSort(a, n); // Function to sort array.
-    printArray(a, n); // Printing the array after sorting.
+    // int a[] = {12, 54, 65, 7, 23, 9};
+    // int n = 6;
+    // printArray(a, n);
+    // insertionSort(a, n);
+    // printf("After Sorting\n");
+    // printArray(a, n);
+
+    int elements, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &elements);
+    int *array = (int *)malloc(elements * sizeof(int));
+    for (i = 0; i < elements; i++){
+        printf("Enter data %d - ", i + 1);
+        scanf("%d", &array[i]);
+    }
+    // printArray(array, elements);
+    insertionSort(array, elements);
+    printf("\nSorted array is: \n");
+    printArray(array, elements); 
     return 0;
 }
--- 2nd Year/Sorting/selection-sort.c
+++ 2nd Year/Sorting/selection-sort.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void printArray(int* A, int n){
     for (int i = 0; i < n; i++)
@@ -10,7 +11,6 @@ void printArray(int* A, int n){
 
 void selectionSort(int *A, int n){
     int indexOfMin, temp;
-    printf("Running Selection sort...\n");
     for (int i = 0; i < n - 1; i++)
     {
         indexOfMin = i;
@@ -20,7 +20,6 @@ void selectionSort(int *A, int n){
                 indexOfMin = j;
             }
         }
-        // Swap A[i] and A[indexOfMin]
         temp = A[i];
         A[i] = A[indexOfMin];
         A[indexOfMin] = temp;
@@ -29,11 +28,23 @@ void selectionSort(int *A, int n){
 
 int main(){
 
-    int A[] = {12, 54, 65, 7, 23, 9};
-    int n = 6;
-    printArray(A, n);
-    selectionSort(A, n);
-    printArray(A, n);
+    // int A[] = {12, 54, 65, 7, 23, 9};
+    // int n = 6;
+    // printArray(A, n);
+    // selectionSort(A, n);
+    // printArray(A, n);
 
+    int elements, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &elements);
+    int *array = (int *)malloc(elements * sizeof(int));
+    for (i = 0; i < elements; i++){
+        printf("Enter data %d - ", i + 1);
+        scanf("%d", &array[i]);
+    }
+    // printArray(array, elements);
+    selectionSort(array, elements);
+    printf("\nSorted array is: \n");
+    printArray(array, elements); 
     return 0;
 }
