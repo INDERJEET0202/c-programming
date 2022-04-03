8431a02313aa18f343426c5dac70155e0414e8c7
indrajit <palindrajit10@gmail.com>
2022-04-03 13:45:59 +0530

    Prim's algorithm

--- 2nd Year/4th SEM/Algorithms/prims_algorithm.c
+++ 2nd Year/4th SEM/Algorithms/prims_algorithm.c
@@ -12,6 +12,7 @@ int minKey(int key[], int mstSet[]){
             min_index = v;
         }
     }
+    printf("%d\n", min_index);
     return min_index;
 }
 
@@ -63,73 +64,3 @@ int main(){
     primes_algo(graph);
     return 0;
 }
-
-
-
-// // Prim's Algorithm in C
-
-// #include<stdio.h>
-// #include<stdbool.h> 
-// #include <string.h>
-
-// #define INF 9999999
-
-// // number of vertices in graph
-// #define V 7
-
-// // create a 2d array of size 5x5
-// //for adjacency matrix to represent graph
-// int G[V][V] = {
-//         {0, 28, 0, 0, 0, 10, 0}, 
-//         {28, 0, 16, 0, 0, 0, 14},
-//         {0, 16, 0, 12, 0, 0, 0},
-//         {0, 0, 12, 0, 22, 0, 18},
-//         {0, 0, 0, 22, 0, 25, 24},
-//         {10, 0, 0, 0, 25, 0, 0},
-//         {0, 14, 0, 0, 18, 24, 0}
-// };
-
-// int main() {
-//   int no_edge;  
-//   int selected[V];
-//   int min_cost = 0;
-
-//   memset(selected, false, sizeof(selected));
-  
-//   no_edge = 0;
-
-//   selected[0] = true;
-
-//   int x; 
-//   int y; 
-
-//   printf("Edge : Weight\n");
-
-//   while (no_edge < V - 1) {
-
-//     int min = INF;
-//     x = 0;
-//     y = 0;
-
-//     for (int i = 0; i < V; i++) {
-//       if (selected[i]) {
-//         for (int j = 0; j < V; j++) {
-//           if (!selected[j] && G[i][j]) { 
-//             if (min > G[i][j]) {
-//               min = G[i][j];
-//               x = i;
-//               y = j;
-//             }
-//           }
-//         }
-//       }
-//     }
-//     printf("%d - %d : %d\n", x, y, G[x][y]);
-//     min_cost = min_cost + min;
-//     selected[y] = true;
-//     no_edge++;
-//   }
-//   printf("\nMinimum cost = %d\n", min_cost);
-
-//   return 0;
-// }
