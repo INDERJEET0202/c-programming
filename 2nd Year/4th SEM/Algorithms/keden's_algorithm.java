405dfb4ecd713792e093c1c58f37295b3dc75d37
indrajit <palindrajit10@gmail.com>
2022-04-09 12:34:53 +0530

    keden's algo for all negetive numbers...

--- 2nd Year/4th SEM/Algorithms/keden's_algorithm.java
+++ 2nd Year/4th SEM/Algorithms/keden's_algorithm.java
@@ -1,14 +1,33 @@
-int maxSumSubarray(int a[]){
-    int maxSum = 0;
-    int currSum = 0;
-    for (int i = 1; i < a.length; i++) {
-        currSum = currSum + a[i];
-        if(currSum < maxSum){
-            maxSum = currSum;
-        }
-        if(currSum < 0){
-            currSum = 0;
-        }
+long maxSubarraySum(int arr[], int n){
+        
+    // Your code here
+long maxSum = 0;
+long currSum = 0;
+for (int i = 0; i < n; i++) {
+    currSum = currSum + arr[i];
+    if(currSum > maxSum){
+        maxSum = currSum;
     }
-    return maxSum;
+    if(currSum < 0){
+        currSum = 0;
+    }
+}
+return maxSum;
+}
+// ******************************************* //
+long maxSubarraySum(int arr[], int n){
+       
+    // Your code here
+    int sum=0;
+    int max_sum=Integer.MIN_VALUE;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        if(max_sum<sum)
+        max_sum=sum;
+        if(sum<0)
+        sum=0;
+        
+    }
+    return max_sum; 
 }
