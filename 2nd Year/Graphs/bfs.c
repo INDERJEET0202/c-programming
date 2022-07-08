a121b98933f48f5fbca677b47bcc7c28c9b12f92
indrajit <palindrajit10@gmail.com>
2022-07-08 23:07:38 +0530

    New BFS

---
+++ 2nd Year/Graphs/bfs.c
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int a[20][20], q[20], visited[20], n, f = 0, r = -1;
+
+void bfs(int v)
+{
+    int i;
+    for (i = 0;i < n;i++)
+    {
+        if (a[v][i] != 0 && visited[i] == 0)
+        {
+            r = r + 1;
+            q[r] = i;
+            visited[i] = 1;
+            printf("%d  ", i);
+        }
+    }
+    f = f + 1;
+    if (f <= r)
+        bfs(q[f]);
+}
+
+void main()
+{
+    int v, i, j, count = 0;
+    printf("\n Enter the number of vertices:");
+    scanf("%d", &n);
+    for (i = 0;i < n;i++)
+    {
+        visited[i] = 0;
+    }
+    printf("\n Enter graph data in matrix form:\n");
+    for (i = 0;i < n;i++)
+        for (j = 0;j < n;j++)
+            scanf("%d", &a[i][j]);
+    printf("\n Enter the starting vertex:");
+    scanf("%d", &v);
+    f = r = 0;
+    q[r] = v;
+    printf("\n BFS traversal is:\n");
+    visited[v] = 1;
+    printf("%d   ", v);
+
+    bfs(v);
+    if (r != n - 1)
+        printf("\n BFS is not possible");
+
+}
+
+// 0
+// 1
+// 0
+// 1
+// 0
+// 0
+// 0
+// 1
+// 0
+// 1
+// 1
+// 0
+// 1
+// 1
+// 0
+// 1
+// 0
+// 1
+// 1
+// 1
+// 0
+// 1
+// 1
+// 1
+// 0
+// 1
+// 0
+// 0
+// 0
+// 0
+// 1
+// 1
+// 0
+// 0
+// 1
+// 0
+// 1
+// 1
+// 0
+// 0
+// 0
+// 0
+// 0
+// 1
+// 0
+// 0
+// 1
+// 0
+// 0
+
+
+
+// 0 1 0 1 0 0 0
+// 1 0 1 1 0 1 1
+// 0 1 0 1 1 1 0
+// 1 1 1 0 1 0 0
+// 0 0 1 1 0 0 1
+// 0 1 1 0 0 0 0
+// 0 1 0 0 1 0 0
